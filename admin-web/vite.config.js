d81750489415de760154bb6f3c534d68247708d7
LICANGHAI <liningjnu@gmail.com>
2022-08-11 17:23:03 +0800

    lint

--- admin-web/vite.config.js
+++ admin-web/vite.config.js
@@ -1,33 +1,23 @@
 import { fileURLToPath, URL } from "node:url"
-
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Components({
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: "sass",
-        }),
-      ],
-    }),
+    Components({ resolvers: [ElementPlusResolver({ importStyle: "sass" })] }),
   ],
   resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+    alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
     // 忽略后缀名的配置选项, 官方不建议省略.vue... -_-#
     extensions: [".mjs", ".js", ".json", ".vue"],
   },
   css: {
     preprocessorOptions: {
-      scss: {
-        additionalData: "@use '@/styles/element/index.scss';",
-      },
+      scss: { additionalData: "@use '@/styles/element/index.scss';" },
     },
   },
   server: {
@@ -36,10 +26,7 @@ export default defineConfig({
     open: true,
     hmr: true,
     proxy: {
-      "/api/pc": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
+      "/api/pc": { target: "http://localhost:8080" },
     },
   },
 })
